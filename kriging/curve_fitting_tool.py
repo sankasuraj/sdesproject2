1aec10a7a470de0ce89e86ef25835b5c9c99ef52
sankasuraj <sankasuraj@gmail.com>
2016-11-20 07:10:55 +0530

    changed GUI code

--- kriging/curve_fitting_tool.py
+++ kriging/curve_fitting_tool.py
@@ -1,7 +1,7 @@
 import Tkinter
 from Tkinter import *
 from tkFileDialog import askopenfilename
-#from Tkinter import messagebox
+from kriging import *
 
 #For running multiple commands using one click
 def combine_funcs(*funcs):
@@ -14,14 +14,11 @@ class simpleapp_tk(Tkinter.Tk):
     def __init__(self,parent):
         Tkinter.Tk.__init__(self,parent)
         self.parent=parent
+        self.filename = None
         self.initialize()
     
     def initialize(self):
         self.grid()        
-        #self.entryVariable = Tkinter.StringVar()
-        #self.entry = Tkinter.Entry(self,textvariable=self.entryVariable)
-        #self.entry.grid(row=0,column=0, columnspan=2, pady=5, padx=5)
-        #self.entryVariable.set("Enter text here.")
         Label(self, text="To Train a model out of a csv file click Train Model").grid(row=0, sticky=W)
         create = Tkinter.Button(self,text="Create Model",command = self.Window2, height = 1, width = 10)
         create.grid(row=1, column=0, sticky='W', padx=5, pady=5)
@@ -36,9 +33,9 @@ class simpleapp_tk(Tkinter.Tk):
 
     #For Opening a file
     def OpenFile(self):
-        filename = askopenfilename()
-        if filename:
-            self.window2.labelVariable.set(filename)
+        self.filename = askopenfilename()
+        if self.filename:
+            self.window2.labelVariable.set(self.filename)
 
     #def Quit(self):
 	#result = messagebox.askyesno("Continue?", "Do you want to quit?")
@@ -58,18 +55,14 @@ class simpleapp_tk(Tkinter.Tk):
         back = Button(self.window2, text="Back",command = self.window2.destroy, height = 1, width = 10)
         back.grid(row=1, column=0, sticky='E',pady=5, padx=5)
 
-        next = Button(self.window2,text="Next",command=combine_funcs(self.Window3,self.window2.destroy,self.Progress), 
-                                height = 1, width = 10)
+        next = Button(self.window2,text="Next",command=combine_funcs(self.Window3,
+            self.window2.destroy), height = 1, width = 10)
         next.grid(row=1, column=1, sticky='E',pady=5, padx=5)
         self.window2.labelVariable = Tkinter.StringVar()
         label2 = Label(self.window2,textvariable=self.window2.labelVariable,
                               anchor="w",fg="white",bg="blue")
         label2.grid(row=2,column=0,columnspan=2,sticky='EW')
         self.window2.labelVariable.set("Hello !")
-
-    def Progress(self):
-        for i in range(10):
-            self.window3.labelVariable.set(i)
 
     def Window3(self):
         self.window3 = Toplevel()
@@ -84,7 +77,48 @@ class simpleapp_tk(Tkinter.Tk):
                               anchor="w",fg="white",bg="blue")
         label3.grid(row=2,column=0,columnspan=2,sticky='EW')
         self.window3.labelVariable.set("Hello !")
+        self.Progress()
         #self.topButton.pack()
+
+    def Progress(self):
+        model_name = '../../model.csv'
+        training_data_file = self.filename
+        start = time()
+        x, y = read_data(training_data_file)
+        x1, y1, x2, y2 = divide_data(x, y)
+        self.window3.labelVariable.set('Preliminary model initialising...')
+        prelim_model = Solve(x1, y1)
+        self.window3.labelVariable.set('Preliminary model initialised')
+        self.window3.labelVariable.set('Preliminary model training started')
+        prelim_model.train()
+        self.window3.labelVariable.set('Preliminary model trained')
+
+        self.window3.labelVariable.set('Estimating error using preliminary model')
+        error = estimate_error(prelim_model, x2, y2)
+        self.window3.labelVariable.set('Error estimation done')
+
+        self.window3.labelVariable.set('Final model initialising...')
+        final_model = Solve(x, y)
+        self.window3.labelVariable.set('Final model initialised')
+        self.window3.labelVariable.set('Final model training started') 
+        self.window3.labelVariable.set('Esimated time remaining is approximately ' 
+            + str(int(1.45 * (time()-start))) + ' seconds' )
+        final_model.train()
+        self.window3.labelVariable.set('Final model trained')
+
+        end = time()
+        self.window3.labelVariable.set('Total time taken = ' + str(int(end-start)) + ' seconds')
+        self.window3.labelVariable.set('L2 error for the given data = ' + str(round(error, 2)))
+
+        f = open(model_name, 'wb')
+        writer = csv.writer(f, delimiter = ',')
+        writer.writerow(np.concatenate([final_model.max_x, final_model.max_y]))
+        writer.writerow(np.concatenate([final_model.min_x, final_model.min_y]))
+        for i in range(final_model.n):
+            writer.writerow(x[i])
+        f.close()
+
+
 
     def Window4(self):
         self.window4 = Toplevel()
@@ -97,7 +131,8 @@ class simpleapp_tk(Tkinter.Tk):
         self.window5 = Toplevel()
         self.window5.title("New Model")
 
-    # app = simpleapp_tk(None)
-    # app.title('Curve Fitting Tool')
+if __name__ == '__main__':
+    app = simpleapp_tk(None)
+    app.title('Curve Fitting Tool')
     # app.geometry("200x200")
-    # app.mainloop()
+    app.mainloop()
--- kriging/main.py
+++
@@ -1,9 +0,0 @@
-from kriging import *
-from curve_fitting_tool import *
-
-if __name__ == '__main__':
-	app = simpleapp_tk(None)
-	app.title('Curve Fitting Tool')
-	app.mainloop()
-	filename = app.window2.labelVariable
-	print filename
